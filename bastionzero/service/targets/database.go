8346c1dfa8a9741457fd8b9877b66379e82174c4
Roshan Bhatia <roshan@timebyping.com>
2023-05-09 15:35:54 -0700

    Add CreateDatabaseTarget to TargetsService

--- bastionzero/service/targets/database.go
+++ bastionzero/service/targets/database.go
@@ -14,6 +14,26 @@ const (
 	databaseSinglePath = databaseBasePath + "/%s"
 )
 
+// CreateDatabaseTargetRequest is used to create a new Database target
+type CreateDatabaseTargetRequest struct {
+	TargetName      string `json:"targetName,omitempty"`
+	ProxyTargetID   string `json:"proxyTargetId,omitempty"`
+	RemoteHost      string `json:"remoteHost,omitempty"`
+	RemotePort      *Port  `json:"remotePort,omitempty"`
+	LocalPort       *Port  `json:"localPort,omitempty"`
+	LocalHost       string `json:"localHost,omitempty"`
+	IsSplitCert     bool   `json:"splitCert,omitempty"`
+	DatabaseType    string `json:"databaseType,omitempty"`
+	EnvironmentID   string `json:"environmentId,omitempty"`
+	EnvironmentName string `json:"environmentName,omitempty"`
+}
+
+// CreateDatabaseTargetRequest is the response returned if a Database target is
+// successfully created
+type CreateDatabaseTargetResponse struct {
+	TargetId string `json:"targetId"`
+}
+
 // ModifyDatabaseTargetRequest is used to modify a Database target
 type ModifyDatabaseTargetRequest struct {
 	TargetName    *string `json:"targetName,omitempty"`
@@ -56,6 +76,25 @@ func (s *TargetsService) ListDatabaseTargets(ctx context.Context) ([]DatabaseTar
 	}
 
 	return *targetList, resp, nil
+}
+
+// CreateDatabaseTarget creates a new Database target.
+//
+// BastionZero API docs: https://cloud.bastionzero.com/api/#post-/api/v2/targets/database
+func (s *TargetsService) CreateDatabaseTarget(ctx context.Context, request *CreateDatabaseTargetRequest) (*CreateDatabaseTargetResponse, *http.Response, error) {
+	u := databaseBasePath
+	req, err := s.Client.NewRequest(ctx, http.MethodPost, u, request)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	createTargetResponse := new(CreateDatabaseTargetResponse)
+	resp, err := s.Client.Do(req, createTargetResponse)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return createTargetResponse, resp, nil
 }
 
 // GetDatabaseTarget fetches the specified Database target.

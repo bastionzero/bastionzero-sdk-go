5fef9836db8c0f1591a86872586a8838c6f75a14
Roshan Bhatia <rshnbhatia@gmail.com>
2023-05-12 07:18:41 -0700

    Add CreateDatabaseTarget and DeleteDatabaseTarget to TargetsService (#16)

--- bastionzero/service/targets/database.go
+++ bastionzero/service/targets/database.go
@@ -14,6 +14,26 @@ const (
 	databaseSinglePath = databaseBasePath + "/%s"
 )
 
+// CreateDatabaseTargetRequest is used to create a new Database target
+type CreateDatabaseTargetRequest struct {
+	TargetName      string `json:"targetName"`
+	ProxyTargetID   string `json:"proxyTargetId"`
+	RemoteHost      string `json:"remoteHost"`
+	RemotePort      Port   `json:"remotePort"`
+	LocalPort       *Port  `json:"localPort,omitempty"`
+	LocalHost       string `json:"localHost,omitempty"`
+	IsSplitCert     bool   `json:"splitCert,omitempty"`
+	DatabaseType    string `json:"databaseType,omitempty"`
+	EnvironmentID   string `json:"environmentId,omitempty"`
+	EnvironmentName string `json:"environmentName,omitempty"`
+}
+
+// CreateDatabaseTargetResponse is the response returned if a Database target is
+// successfully created
+type CreateDatabaseTargetResponse struct {
+	TargetId string `json:"targetId"`
+}
+
 // ModifyDatabaseTargetRequest is used to modify a Database target
 type ModifyDatabaseTargetRequest struct {
 	TargetName    *string `json:"targetName,omitempty"`
@@ -58,6 +78,25 @@ func (s *TargetsService) ListDatabaseTargets(ctx context.Context) ([]DatabaseTar
 	return *targetList, resp, nil
 }
 
+// CreateDatabaseTarget creates a new Database target.
+//
+// BastionZero API docs: https://cloud.bastionzero.com/api/#post-/api/v2/targets/database
+func (s *TargetsService) CreateDatabaseTarget(ctx context.Context, request *CreateDatabaseTargetRequest) (*CreateDatabaseTargetResponse, *http.Response, error) {
+	u := databaseBasePath
+	req, err := s.Client.NewRequest(ctx, http.MethodPost, u, request)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	createTargetResponse := new(CreateDatabaseTargetResponse)
+	resp, err := s.Client.Do(req, createTargetResponse)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return createTargetResponse, resp, nil
+}
+
 // GetDatabaseTarget fetches the specified Database target.
 //
 // BastionZero API docs: https://cloud.bastionzero.com/api/#get-/api/v2/targets/database/-id-
@@ -75,6 +114,24 @@ func (s *TargetsService) GetDatabaseTarget(ctx context.Context, targetID string)
 	}
 
 	return target, resp, nil
+}
+
+// DeleteDatabaseTarget deletes the specified Database target.
+//
+// BastionZero API docs: https://cloud.bastionzero.com/api/#delete-/api/v2/targets/database/-id-
+func (s *TargetsService) DeleteDatabaseTarget(ctx context.Context, targetID string) (*http.Response, error) {
+	u := fmt.Sprintf(databaseSinglePath, targetID)
+	req, err := s.Client.NewRequest(ctx, http.MethodDelete, u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := s.Client.Do(req, nil)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
 }
 
 // ModifyDatabaseTarget updates a Database target.

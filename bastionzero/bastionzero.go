1f96fed33495e4d632f5ddc18c6c84654de85976
Yuval Marcus <ymarcus93@gmail.com>
2023-05-18 13:07:24 -0400

    Add support for mfa endpoints

--- bastionzero/bastionzero.go
+++ bastionzero/bastionzero.go
@@ -18,6 +18,7 @@ import (
 	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/environments"
 	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/events"
 	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/githubactions"
+	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/mfa"
 	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/organization"
 	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/policies"
 	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/serviceaccounts"
@@ -60,6 +61,7 @@ type Client struct {
 	Environments         *environments.EnvironmentsService
 	Events               *events.EventsService
 	GitHubActions        *githubactions.GitHubActionsService
+	MFA                  *mfa.MFAService
 	Organization         *organization.OrganizationService
 	Policies             *policies.PoliciesService
 	ServiceAccounts      *serviceaccounts.ServiceAccountsService
@@ -90,6 +92,7 @@ func NewClient(httpClient *http.Client) *Client {
 	c.Environments = (*environments.EnvironmentsService)(&c.common)
 	c.Events = (*events.EventsService)(&c.common)
 	c.GitHubActions = (*githubactions.GitHubActionsService)(&c.common)
+	c.MFA = (*mfa.MFAService)(&c.common)
 	c.Organization = (*organization.OrganizationService)(&c.common)
 	c.Policies = (*policies.PoliciesService)(&c.common)
 	c.ServiceAccounts = (*serviceaccounts.ServiceAccountsService)(&c.common)

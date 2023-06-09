45424fca4ee17e39f8b9291114f5a8a8d0dca400
johncmerfeld <john.merfeld@gmail.com>
2023-06-09 16:23:19 -0400

    init

--- bastionzero/service/autodiscoveryscripts/bzero.go
+++ bastionzero/service/autodiscoveryscripts/bzero.go
@@ -24,11 +24,36 @@ type BzeroBashAutodiscoveryScript struct {
 }
 
 // GetBzeroBashAutodiscoveryScript gets a bash script that can be used to
-// install the latest production BastionZero agent (bzero) on your targets.
+// install the latest production BastionZero agent (bzero) on your Linux targets.
 //
 // BastionZero API docs: https://cloud.bastionzero.com/api/#get-/api/v2/autodiscovery-scripts/bzero/bash
 func (s *AutodiscoveryScriptsService) GetBzeroBashAutodiscoveryScript(ctx context.Context, opts *BzeroBashAutodiscoveryOptions) (*BzeroBashAutodiscoveryScript, *http.Response, error) {
 	u := autodiscoveryScriptsBasePath + "/bzero/bash"
+	u, err := client.AddOptions(u, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.Client.NewRequest(ctx, http.MethodGet, u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	script := new(BzeroBashAutodiscoveryScript)
+	resp, err := s.Client.Do(req, script)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return script, resp, nil
+}
+
+// GetBzeroPowershellAutodiscoveryScript gets a powershell script that can be used to
+// install the latest production BastionZero agent (bzero) on your Windows targets.
+//
+// BastionZero API docs: https://cloud.bastionzero.com/api/#get-/api/v2/autodiscovery-scripts/bzero/bash
+func (s *AutodiscoveryScriptsService) GetBzeroPowershellAutodiscoveryScript(ctx context.Context, opts *BzeroBashAutodiscoveryOptions) (*BzeroBashAutodiscoveryScript, *http.Response, error) {
+	u := autodiscoveryScriptsBasePath + "/windows/powershell"
 	u, err := client.AddOptions(u, opts)
 	if err != nil {
 		return nil, nil, err

11ca946e9cc13fb200968ca538ee00fdf107bd4a
johncmerfeld <john.merfeld@gmail.com>
2023-06-19 11:07:58 -0400

    address feedback

--- bastionzero/service/autodiscoveryscripts/bzero.go
+++ bastionzero/service/autodiscoveryscripts/bzero.go
@@ -9,7 +9,7 @@ import (
 )
 
 // BzeroBashAutodiscoveryOptions specifies the required query-string parameters
-// in order to get a Bzero bash autodiscovery script
+// in order to get a Linux bash autodiscovery script
 type BzeroBashAutodiscoveryOptions struct {
 	// TargetNameOption specifies the target name schema option. Required.
 	TargetNameOption targetnameoption.TargetNameOption `url:"targetNameOption"`
@@ -19,7 +19,22 @@ type BzeroBashAutodiscoveryOptions struct {
 	EnvironmentID string `url:"environmentId"`
 }
 
+// WindowsPowershellAutodiscoveryOptions specifies the required query-string parameters
+// in order to get a Windows Powershell autodiscovery script
+type WindowsPowershellAutodiscoveryOptions struct {
+	// TargetNameOption specifies the target name schema option. Required.
+	TargetNameOption targetnameoption.TargetNameOption `url:"targetNameOption"`
+
+	// EnvironmentID is the unique ID for the environment the target should
+	// associate with. Required.
+	EnvironmentID string `url:"environmentId"`
+}
+
 type BzeroBashAutodiscoveryScript struct {
+	Script string `json:"autodiscoveryScript"`
+}
+
+type WindowsPowershellAutodiscoveryScript struct {
 	Script string `json:"autodiscoveryScript"`
 }
 
@@ -51,8 +66,8 @@ func (s *AutodiscoveryScriptsService) GetBzeroBashAutodiscoveryScript(ctx contex
 // GetBzeroPowershellAutodiscoveryScript gets a powershell script that can be used to
 // install the latest production BastionZero agent (bzero) on your Windows targets.
 //
-// BastionZero API docs: https://cloud.bastionzero.com/api/#get-/api/v2/autodiscovery-scripts/bzero/bash
-func (s *AutodiscoveryScriptsService) GetBzeroPowershellAutodiscoveryScript(ctx context.Context, opts *BzeroBashAutodiscoveryOptions) (*BzeroBashAutodiscoveryScript, *http.Response, error) {
+// BastionZero API docs: https://cloud.bastionzero.com/api/#get-/api/v2/autodiscovery-scripts/windows/powershell
+func (s *AutodiscoveryScriptsService) GetBzeroPowershellAutodiscoveryScript(ctx context.Context, opts *WindowsPowershellAutodiscoveryOptions) (*WindowsPowershellAutodiscoveryScript, *http.Response, error) {
 	u := autodiscoveryScriptsBasePath + "/windows/powershell"
 	u, err := client.AddOptions(u, opts)
 	if err != nil {
@@ -64,7 +79,7 @@ func (s *AutodiscoveryScriptsService) GetBzeroPowershellAutodiscoveryScript(ctx 
 		return nil, nil, err
 	}
 
-	script := new(BzeroBashAutodiscoveryScript)
+	script := new(WindowsPowershellAutodiscoveryScript)
 	resp, err := s.Client.Do(req, script)
 	if err != nil {
 		return nil, resp, err

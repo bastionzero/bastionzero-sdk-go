2b1a2b7b8956d71a208c203f700e1e8197f00e04
Chad Unrein <chad@commonwealthcrypto.com>
2023-10-11 12:29:44 -0500

    add omitempty where missing and changie changes

--- bastionzero/service/targets/dbauthconfig/dbauthconfig.go
+++ bastionzero/service/targets/dbauthconfig/dbauthconfig.go
@@ -6,8 +6,8 @@ package dbauthconfig
 // /api/v2/targets/database/supported-database-configs and then one of the returned
 // configurations is used in any subsequent create or update request as needed.
 type DatabaseAuthenticationConfig struct {
-	AuthenticationType   *string `json:"authenticationType"`
-	CloudServiceProvider *string `json:"cloudServiceProvider"`
-	Database             *string `json:"database"`
-	Label                *string `json:"label"`
+	AuthenticationType   *string `json:"authenticationType,omitempty"`
+	CloudServiceProvider *string `json:"cloudServiceProvider,omitempty"`
+	Database             *string `json:"database,omitempty"`
+	Label                *string `json:"label,omitempty"`
 }
